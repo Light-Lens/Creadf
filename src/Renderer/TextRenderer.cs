959d20b065dc14b010b270a515b4cbd929208740
SrijanSriv211 <QCoreNest@gmail.com>
2024-06-19 14:25:17 +0530

    Make tokens to render white at the beginning and after semicolon

--- src/Renderer/TextRenderer.cs
+++ src/Renderer/TextRenderer.cs
@@ -3,6 +3,10 @@ partial class Creadf
     private (int, int) DiffTokenIdx = (0, 0);
     private string RenderedTextBuffer = "";
     private Tokenizer tokenizer;
+    // White points those points on which the renderer has to render the tokens with white color.
+    // Such as the very first token is to be rendered in white color as well as tokens which successed the semicolon.
+    // This doesn't include whitespaces and only applies on renderable tokens.
+    private List<int> WhitePoints = [];
 
     private void UpdateBuffer(bool RenderSuggestions=true)
     {
@@ -52,20 +56,51 @@ partial class Creadf
 
         // Check if the token is to be highlighted or not. If yes, then highlight.
         string Token = token.Name[char_idx..];
-        if (Config.ToggleColorCoding && token_idx == 0)
-            Terminal.Print(Token, ConsoleColor.White, false);
 
-        else if (Config.ToggleColorCoding && Config.SyntaxHighlightCodes.TryGetValue(token.Type, out ConsoleColor color))
+        if (Config.ToggleColorCoding && Config.SyntaxHighlightCodes.TryGetValue(token.Type, out ConsoleColor color))
             Terminal.Print(Token, color, false);
+
+        else if (Config.ToggleColorCoding && WhitePoints.Any(x => token_idx == x))
+            Terminal.Print(Token, ConsoleColor.White, false);
 
         // Otherwise update text after cursor normally.
         else
             Console.Write(Token);
     }
 
+    // Get all the white points
+    private void GetWhitePoints()
+    {
+        Tokenizer.TokenType[] tokens = tokenizer.tokens.Select(x => x.Type).ToArray();
+        bool PauseAddingPoints = false;
+        WhitePoints = [];
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            Tokenizer.TokenType TokType = tokens[i];
+            if (TokType == Tokenizer.TokenType.EOL)
+            {
+                WhitePoints.Add(i);
+                break;
+            }
+
+            else if (TokType != Tokenizer.TokenType.WHITESPACE && TokType != Tokenizer.TokenType.SEMICOLON && !PauseAddingPoints)
+            {
+                WhitePoints.Add(i);
+                PauseAddingPoints = true;
+            }
+
+            else if (TokType == Tokenizer.TokenType.SEMICOLON && PauseAddingPoints)
+                PauseAddingPoints = false;
+        }
+    }
+
     // Render the updated input with syntax highlighting
     private void RenderTextBuffer()
     {
+        // Get all the white points
+        GetWhitePoints();
+
         // Loop through each token starting from first different token
         RenderToken(DiffTokenIdx.Item1, DiffTokenIdx.Item2);
         for (int i = DiffTokenIdx.Item1 + 1; i < tokenizer.tokens.Count; i++)
